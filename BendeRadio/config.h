e4ba5426966625bfb616551c2d78c545a2fb1e81
Maxim Kurilyak <maxim.kurilyak@yandex.ru>
2023-12-21 08:23:00 +0600

    Add files via upload

---
+++ BendeRadio/config.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#define AP_SSID "iPhone"
+#define AP_PASS "mm@5289Max"
+//#define AP_SSID "RT-GPON-C71C"
+//#define AP_PASS "kEp3X4AP"
+
+#define I2S_DOUT 25
+#define I2S_BCLK 27
+#define I2S_LRC 26
+
+#define MTRX_CS 22
+#define MTRX_DAT 23
+#define MTRX_CLK 21
+
+#define ENC_S1 19
+#define ENC_S2 18
+#define ENC_BTN 5
+
+#define ANALYZ_PIN 34
+
+#define ANALYZ_WIDTH (3 * 8)
+#define RADIO_BUFFER (1600 * 25)  // default 1600*5, этого МАЛО
